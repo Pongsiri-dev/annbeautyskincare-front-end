1d4809d21b698f5f8ed688e7faec5e99819b95c8
Supanut <supanut.suantawee@gmail.com>
2022-01-07 16:04:56 +0700

    export card

--- src/components/EmployeeCard.tsx
+++ src/components/EmployeeCard.tsx
@@ -1,8 +1,16 @@
 // @mui
-import { Link, Card, Typography, CardHeader, Stack } from "@mui/material";
-import { useEffect, useState } from "react";
+import {
+  Link,
+  Card,
+  Typography,
+  CardHeader,
+  Stack,
+  Button,
+} from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 import { UserAbout } from "src/@types/user";
 import styled from "styled-components";
+import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
 // @types
 // components
 
@@ -42,6 +50,11 @@ const WrapperStyle = styled.div`
   @font-face {
     font-family: DB-Heavent-Cond;
     src: url(/fonts/card/DB-Heavent-Cond.ttf);
+  }
+  @font-face {
+    font-family: DB-Heavent-Cond;
+    font-weight: bold;
+    src: url(/fonts/card/DB-Heavent-Bd-Cond.ttf);
   }
   /* 
 img-fluid d-block mx-auto 
@@ -478,6 +491,8 @@ export default function EmployeeCard({ profile }: Props) {
     postCode,
   } = profile;
 
+  const pdfExportComponent = useRef(null);
+
   const [type, setType] = useState<string>("");
 
   useEffect(() => {
@@ -493,109 +508,121 @@ export default function EmployeeCard({ profile }: Props) {
     }
   }, [level]);
 
+  const handleExportWithComponent = () => {
+    const { current }: any = pdfExportComponent;
+    if (current) {
+      current.save();
+    }
+  };
+
   return (
     <WrapperStyle>
-      {level === "Platinum" ? (
-        <div className="col bg-card">
-          <div className="card platinum front">
-            <img src="/company/IMG-3075.png" className="logo" />
-            <h5>บริษัท 776/112 พัฒนาการ38 หมู่บ้านเดอะคอนเนค </h5>
-            <h6>
-              โทร <span>0955542399, 0886659142</span>
-            </h6>
-          </div>
-
-          <br />
-
-          <div className="card platinum back">
-            <h2>super dealer</h2>
-            <h3>ชุปเปอร์ ดีลเลอร์ วาเลนต้า คอฟฟี่ </h3>
-            <div className="card-text">
-              <h5>
-                Name
-                <span>
-                  {firstName} {lastName}
-                </span>
-              </h5>
-              <h5>
-                รหัส <span> xxxx xxxxxx</span>
-              </h5>
-              <h5>
-                สายงาน <span> xxxx xxxxxx</span>
-              </h5>
-              <h5>
-                เบอร์โทร <span> {telephone}</span>
-              </h5>
-            </div>
-            <div className="card-text">
+      <Stack justifyContent="center">
+        <Button onClick={handleExportWithComponent}>Export Card</Button>
+      </Stack>
+      <PDFExport ref={pdfExportComponent} fileName="member-card">
+        {level === "Platinum" ? (
+          <div className="col bg-card">
+            <div className="card platinum front">
+              <img src="/company/IMG-3075.png" className="logo" />
+              <h5>บริษัท 776/112 พัฒนาการ38 หมู่บ้านเดอะคอนเนค </h5>
               <h6>
-                <img src="/company/facebook.png" alt="" />{" "}
-                <span> xxxx xxxxxx</span>
-              </h6>
-              <h6>
-                <img src="/company/line1.png" alt="" />{" "}
-                <span> xxxx xxxxxx</span>
-              </h6>
-              <h6>
-                <img src="/company/instagram.png" alt="" />{" "}
-                <span> xxxx xxxxxx</span>
+                โทร <span>0955542399, 0886659142</span>
               </h6>
             </div>
-          </div>
-        </div>
-      ) : (
-        <div className="col bg-card">
-          <div className={type + " card card-type front"}>
-            <img src="/company/IMG-3075.png" className="logo" />
-            <h5>บริษัท 776/112 พัฒนาการ38 หมู่บ้านเดอะคอนเนค </h5>
-            <h6>
-              โทร <span>0955542399, 0886659142</span>
-            </h6>
-          </div>
-          <br />
-          <div className={type + " card card-type back"}>
-            <img src="/company/IMG-5218.png" className="logo" />
-            <h2>
-              <span>
-                คุณ {firstName} {lastName}
-              </span>
-            </h2>
-            <h6>ชุปเปอร์ ดีลเลอร์ โกล วาเลนต้า คอฟฟี่ </h6>
-            <ul>
-              <li>
-                <p>สายงาน </p>
-                <span>xxxxx</span>
-              </li>
-              <li>
-                <p>
+
+            <br />
+
+            <div className="card platinum back">
+              <h2>super dealer</h2>
+              <h3>ชุปเปอร์ ดีลเลอร์ วาเลนต้า คอฟฟี่ </h3>
+              <div className="card-text">
+                <h5>
+                  Name
+                  <span>
+                    {firstName} {lastName}
+                  </span>
+                </h5>
+                <h5>
+                  รหัส <span> xxxx xxxxxx</span>
+                </h5>
+                <h5>
+                  สายงาน <span> xxxx xxxxxx</span>
+                </h5>
+                <h5>
+                  เบอร์โทร <span> {telephone}</span>
+                </h5>
+              </div>
+              <div className="card-text">
+                <h6>
                   <img src="/company/facebook.png" alt="" />{" "}
-                </p>
-                <span>xxxxx</span>
-              </li>
-              <li>
-                <p>รหัส </p>
-                <span>xxxxx</span>
-              </li>
-              <li>
-                <p>
+                  <span> xxxx xxxxxx</span>
+                </h6>
+                <h6>
                   <img src="/company/line1.png" alt="" />{" "}
-                </p>
-                <span>xxxxx</span>
-              </li>
-              <li>
-                <p>เบอร์โทร </p>
-                <span>{telephone}</span>
-              </li>
-              <li>
-                <p>
+                  <span> xxxx xxxxxx</span>
+                </h6>
+                <h6>
                   <img src="/company/instagram.png" alt="" />{" "}
-                </p>
-                <span>xxxxx</span>
-              </li>
-            </ul>
+                  <span> xxxx xxxxxx</span>
+                </h6>
+              </div>
+            </div>
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="col bg-card">
+            <div className={type + " card card-type front"}>
+              <img src="/company/IMG-3075.png" className="logo" />
+              <h5>บริษัท 776/112 พัฒนาการ38 หมู่บ้านเดอะคอนเนค </h5>
+              <h6>
+                โทร <span>0955542399, 0886659142</span>
+              </h6>
+            </div>
+            <br />
+            <div className={type + " card card-type back"}>
+              <img src="/company/IMG-5218.png" className="logo" />
+              <h2>
+                <span>
+                  คุณ {firstName} {lastName}
+                </span>
+              </h2>
+              <h6>ชุปเปอร์ ดีลเลอร์ โกล วาเลนต้า คอฟฟี่ </h6>
+              <ul>
+                <li>
+                  <p>สายงาน </p>
+                  <span>xxxxx</span>
+                </li>
+                <li>
+                  <p>
+                    <img src="/company/facebook.png" alt="" />{" "}
+                  </p>
+                  <span>xxxxx</span>
+                </li>
+                <li>
+                  <p>รหัส </p>
+                  <span>xxxxx</span>
+                </li>
+                <li>
+                  <p>
+                    <img src="/company/line1.png" alt="" />{" "}
+                  </p>
+                  <span>xxxxx</span>
+                </li>
+                <li>
+                  <p>เบอร์โทร </p>
+                  <span>{telephone}</span>
+                </li>
+                <li>
+                  <p>
+                    <img src="/company/instagram.png" alt="" />{" "}
+                  </p>
+                  <span>xxxxx</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+      </PDFExport>
     </WrapperStyle>
   );
 }
