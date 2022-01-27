f02a7e3e60f243be57d8d3d1ca5c292dbfb0f518
Supanut <supanut.suantawee@gmail.com>
2022-01-27 23:23:09 +0700

    sort user list

--- src/pages/dashboard/UserList.tsx
+++ src/pages/dashboard/UserList.tsx
@@ -226,6 +226,15 @@ export default function UserList() {
     }
   }, [userList]);
 
+  useEffect(() => {
+    let obk = applySortFilter(
+      userList,
+      getComparator(order, orderBy),
+      filterName
+    );
+    setFilteredUsers(obk);
+  }, [filterName, order, orderBy]);
+
   return (
     <Page title="รายชื่อลูกทีม">
       <Container maxWidth={themeStretch ? false : "lg"}>
