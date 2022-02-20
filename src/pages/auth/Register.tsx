7ddf1cd7f1705cb95229c3b7eeccd2205ac0af50
Pongsiri-dev <Taa_wappey@windowslive.com>
2022-02-20 23:56:57 +0700

    add link

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,60 +1,60 @@
-import { capitalCase } from 'change-case';
-import { Link as RouterLink } from 'react-router-dom';
+import { capitalCase } from "change-case";
+import { Link as RouterLink } from "react-router-dom";
 // @mui
-import { styled } from '@mui/material/styles';
-import { Box, Card, Link, Container, Typography, Tooltip } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Box, Card, Link, Container, Typography, Tooltip } from "@mui/material";
 // hooks
-import useAuth from '../../hooks/useAuth';
-import useResponsive from '../../hooks/useResponsive';
+import useAuth from "../../hooks/useAuth";
+import useResponsive from "../../hooks/useResponsive";
 // routes
-import { PATH_AUTH } from '../../routes/paths';
+import { PATH_AUTH } from "../../routes/paths";
 // components
-import Page from '../../components/Page';
-import Logo from '../../components/Logo';
-import Image from '../../components/Image';
+import Page from "../../components/Page";
+import Logo from "../../components/Logo";
+import Image from "../../components/Image";
 // sections
-import { RegisterForm } from '../../sections/auth/register';
-import UserNewForm from '../../sections/auth/register/RegisterForm';
+import { RegisterForm } from "../../sections/auth/register";
+import UserNewForm from "../../sections/auth/register/RegisterForm";
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
+const RootStyle = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
   },
 }));
 
-const HeaderStyle = styled('header')(({ theme }) => ({
+const HeaderStyle = styled("header")(({ theme }) => ({
   top: 0,
   zIndex: 9,
   lineHeight: 0,
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  position: 'absolute',
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  position: "absolute",
   padding: theme.spacing(3),
-  justifyContent: 'space-between',
-  [theme.breakpoints.up('md')]: {
-    alignItems: 'flex-start',
+  justifyContent: "space-between",
+  [theme.breakpoints.up("md")]: {
+    alignItems: "flex-start",
     padding: theme.spacing(7, 5, 0, 7),
   },
 }));
 
 const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
+  width: "100%",
   maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   margin: theme.spacing(2, 0, 2, 2),
 }));
 
-const ContentStyle = styled('div')(({ theme }) => ({
-  margin: 'auto',
-  display: 'flex',
-  minHeight: '100vh',
-  flexDirection: 'column',
-  justifyContent: 'center',
+const ContentStyle = styled("div")(({ theme }) => ({
+  margin: "auto",
+  display: "flex",
+  minHeight: "100vh",
+  flexDirection: "column",
+  justifyContent: "center",
   padding: theme.spacing(12, 0),
 }));
 
@@ -63,18 +63,31 @@ const ContentStyle = styled('div')(({ theme }) => ({
 export default function Register() {
   const { method } = useAuth();
 
-  const smUp = useResponsive('up', 'sm');
-  const mdUp = useResponsive('up', 'md');
+  const smUp = useResponsive("up", "sm");
+  const mdUp = useResponsive("up", "md");
 
   return (
     <Page title="Register">
       <RootStyle>
         <HeaderStyle>
           <Logo />
+          <Typography variant="body2" sx={{ mt: { md: -2 } }}>
+            ลดความระเอียดภาพ {""}
+            <a
+              href="https://imagecompressor.io/compress-to-exact-size"
+              target={"_bank"}
+            >
+              ลิ้งค์
+            </a>
+          </Typography>
           {smUp && (
             <Typography variant="body2" sx={{ mt: { md: -2 } }}>
-              เข้าสู่ระบบด้วยบัญชีที่มีอยู่แล้ว {''}
-              <Link variant="subtitle2" component={RouterLink} to={PATH_AUTH.login}>
+              เข้าสู่ระบบด้วยบัญชีที่มีอยู่แล้ว {""}
+              <Link
+                variant="subtitle2"
+                component={RouterLink}
+                to={PATH_AUTH.login}
+              >
                 เข้าสู่ระบบ
               </Link>
             </Typography>
@@ -97,11 +110,23 @@ export default function Register() {
           <ContentStyle>
             {/* เรียกใช้ Form */}
             <UserNewForm isEdit={false} />
-
+            <Typography variant="body2" sx={{ mt: { md: -2 } }}>
+              ลดความระเอียดภาพ {""}
+              <a
+                href="https://imagecompressor.io/compress-to-exact-size"
+                target={"_bank"}
+              >
+                ลิ้งค์
+              </a>
+            </Typography>
             {!smUp && (
-              <Typography variant="body2" sx={{ mt: 3, textAlign: 'center' }}>
-                เข้าสู่ระบบด้วยบัญชีที่มีอยู่แล้ว{' '}
-                <Link variant="subtitle2" to={PATH_AUTH.login} component={RouterLink}>
+              <Typography variant="body2" sx={{ mt: 3, textAlign: "center" }}>
+                เข้าสู่ระบบด้วยบัญชีที่มีอยู่แล้ว{" "}
+                <Link
+                  variant="subtitle2"
+                  to={PATH_AUTH.login}
+                  component={RouterLink}
+                >
                   เข้าสู่ระบบ
                 </Link>
               </Typography>
